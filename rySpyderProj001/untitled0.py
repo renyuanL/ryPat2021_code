e340e8731c2da242a15494fab105fb26849d26dc
Renyuan Lyu <renyuan.lyu@gmail.com>
2021-09-28 09:23:17 +0800

    2021.09.28 HomeWorkDemo
    ....

---
+++ rySpyderProj001/untitled0.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Ch01: Linear Algebra
+
+Created on Sat Sep 25 17:36:35 2021
+
+@author: renyu
+
+
+"""
+
+x= 1
+y= 1.0
+z= 1+1j
+#%%
+aList= [1,2,3]
+aSet=  {10,20,30}
+aDict= {'a':1, 'b':2, 'c':3}
+#%%
+import numpy as np
+
+v= [10, 20, 30]
+v= np.array(v)
+
+
+A= [[1, 2, 3],
+    [4, 5, 6]]
+
+A= np.array(A)
+#%%
+v1= [10, 20, 30]
+v2= [40, 50, 60]
+
+v1= np.array(v1)
+v2= np.array(v2)
+#%%
