471701e45a80aa995f1c3e2e8f0bad2f5097a593
Renyuan Lyu <renyuan.lyu@gmail.com>
2021-10-18 17:57:03 +0800

    ...

---
+++ Master-Math-by-Coding-in-Python-main/.ipynb_checkpoints/ryPat-checkpoint.py
@@ -0,0 +1,17 @@
+import numpy
+import numpy as np
+
+import sympy
+import sympy as sm
+from   sympy.abc import *
+
+import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
+
+import seaborn as sb
+sb.set_style('whitegrid')
+
+from IPython.display import display, Math
+
+#%matplotlib qt
+#%matplotlib inline
